2cd4f9a01eff5264e00fca98876409cae61b29b6
Matheus <M>
2018-02-26 17:40:11 -0300

    [2018-02-26 17:39] first commit

---
+++ SQLRelationalDivisionJava/src/sqlrelationaldivisionjava/RelationalDivision.java
@@ -0,0 +1,15 @@
+package sqlrelationaldivisionjava;
+/**
+ *
+ * @author Matheus Ladislau
+ */
+public class RelationalDivision{
+    
+    public double[][] relationalDivision(double[][] venda,double[][] itemVenda,int[] position){
+        
+        //copy result matrix in correct lenght
+        double[][] result=new double[0][0];
+        return result;
+    } 
+    
+}
