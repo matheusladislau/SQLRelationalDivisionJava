b8215daaaf324c4c0233ecca7c3648d9a03a2442
Matheus <M>
2018-02-27 17:06:36 -0300

    [2018-02-27 17:06]

--- SQLRelationalDivisionJava/src/sqlrelationaldivisionjava/Main.java
+++ SQLRelationalDivisionJava/src/sqlrelationaldivisionjava/Main.java
@@ -10,6 +10,115 @@ package sqlrelationaldivisionjava;
  */
 public class Main{
     public static void main(String[] args){
-    
-    }    
+        RelationalDivision d=new RelationalDivision();
+//
+        double[][] venda=new double[4][2];
+        for(int i=0; i<venda.length; i++){
+            venda[i][0]=(i+1);
+        }       
+        double[][] produto=new double[9][2];
+        for(int i=0; i<produto.length; i++){
+            produto[i][0]=(i+1);
+        }
+        double[][]itemVenda={
+            {1,1,7},
+            {1,9,2},
+            {2,3,2},
+            {2,9,2},
+            {2,1,7},
+            {3,4,2},
+            {3,5,2},
+            {3,5,2},
+            {3,1,7},
+            {3,9,7},
+            {4,1,7},
+            {4,2,7},
+            {4,2,2},
+            {4,2,3},
+            {4,2,4},
+            {4,2,5},
+            {4,2,6},
+        };
+        
+//FIM POPULA
+        System.out.println("PRODUTO");
+        print(produto);
+        System.out.println("");       
+        System.out.println("VENDA");
+        print(venda);          
+        System.out.println("");
+        System.out.println("ITEMVENDA");
+        print(itemVenda);      
+        System.out.println("=================");
+
+        System.out.println("VENDA/ITEMVENDA:");
+        System.out.println("RESULTADO DIVISÃO: ");
+        print(d.relationalDivision(venda,itemVenda,new int[]{0,0}));
+        System.out.println("=============");
+//TESTES
+        
+        double[][]teste={
+            {1,1},
+            {1,2},
+            {2,3},
+            {3,3},
+            {1,3},
+        };
+        double[][]teste1={
+            {0,1},
+            {0,2},
+            {0,3},
+        };
+        System.out.println("DIVISAO TESTE 1"); 
+        print(d.relationalDivision(teste1,teste,new int[]{1,0}));
+        System.out.println("=============");
+        
+        double[][]testeCF={
+            {1,1},
+            {1,2},
+            {1,3},
+            {1,4},
+            {1,5},
+            {1,6},
+            {2,5},
+            {2,6},
+        };
+        double[][]testeF={
+            {1,},
+            {2,},
+            {3,},
+            {4,},
+            {5,},
+            {6,},
+        };
+        System.out.println("DIVISAO TESTE CF"); 
+        print(d.relationalDivision(testeF,testeCF,new int[]{0,1}));
+        System.out.println("=============");
+        
+        
+        double[][]inscr={
+            {1090,1},
+            {1090,2},
+            {1080,1},
+            {1070,1},
+            {1060,1},
+            {1060,2},
+        };
+        double[][]disciplina={
+            {1,100},
+            {2,200},
+        };
+        System.out.println("DIVISAO INSC/DISC"); 
+        print(d.relationalDivision(disciplina,inscr,new int[]{0,1}));
+        System.out.println("=============");      
+    }
+
+    public static void print(double[][] x){
+        for (int i = 0; i <x.length; i++) {
+            for (int j = 0; j <x[i].length; j++) {
+                System.out.print(x[i][j]+"   ");
+            }
+            System.out.println("");
+        }
+    }
 }
--- SQLRelationalDivisionJava/src/sqlrelationaldivisionjava/RelationalDivision.java
+++ SQLRelationalDivisionJava/src/sqlrelationaldivisionjava/RelationalDivision.java
@@ -4,12 +4,122 @@ package sqlrelationaldivisionjava;
  * @author Matheus Ladislau
  */
 public class RelationalDivision{
+    //SQLRelationalDivisionJava
+    //SQL Relational Division in Java using Matrices
     
+    /**
+     * 
+     * @param venda 
+     * @param itemVenda 
+     * @param position a vetor with the position of the primary keys from venda and itemvenda
+     * @return the result of SQL Relational Division
+     */
     public double[][] relationalDivision(double[][] venda,double[][] itemVenda,int[] position){
+        int pos1=position[0];
+        int pos2=position[1];   
+        int keyIndex=0;
+        //create and auxiliar vetor with unique keys
+        //cria vetor auxiliar para que contem as chaves sem repetição de chave
+        double[] auxChave=new double[venda.length];
+        for(int i=0; i<venda.length; i++){
+            boolean exist=false;
+            for(int j=0; j<=i; j++){
+                //verify if the element exists to remove repetition
+                //verifica se elemento existe para eliminar repetição
+                if(venda[i][pos1]==auxChave[j])
+                    exist=true;
+                if((!exist)&&(j==i)){
+                    auxChave[keyIndex]=venda[i][pos1];
+                    keyIndex++;                        
+                }
+            }
+        }
+        //create a vector that copy
+        //cria vetor venda unica que copia item venda com tamanho correto (sem repetição de valores)
+        double[] chaveUnica=new double[keyIndex];
+        for(int i=0; i<chaveUnica.length; i++){
+            chaveUnica[i]=auxChave[i];
+        }
         
+        //create a copy of matrix without repetition
+        //Cria uma cópia da matriz itemVenda sem repetição 
+        double[][] copiaItemVenda=itemVenda.clone();
+        for(int i=0; i<copiaItemVenda.length; i++){
+            for (int j=i+1; j<copiaItemVenda.length; j++){
+                int counter=0;
+                for (int k=0; k<copiaItemVenda[0].length; k++){
+                    if(copiaItemVenda[i][k]==copiaItemVenda[j][k]){
+                        counter++;
+                        if(counter==copiaItemVenda[0].length){
+                            for(int l=0; l<copiaItemVenda[0].length; l++){
+                                copiaItemVenda[j][l]=666; //aponta que linha não existe
+                            }
+                        }
+                    }
+               }            
+            }
+        }
+        //get index of new matrix without repetition
+        //pega indice da nova matriz sem repetição
+        int copiaLength=copiaItemVenda.length;
+        for(int i=0; i<copiaItemVenda.length; i++){
+            if(copiaItemVenda[i][0]==-666)
+                copiaLength--; //caso não exista, decrescer
+        }
+        //new matrix without repetition of values
+        //nova matriz sem repetição de valores
+        double[][] itemVendaUnico=new double[copiaLength][copiaItemVenda[0].length];
+        for (int i=0,j=0; i<copiaItemVenda.length; i++) {
+            if(copiaItemVenda[i][0]!=-1){
+                itemVendaUnico[j]=copiaItemVenda[i];
+                j++;
+            }
+        }
+        
+        //new auxiliar matrix without key's column 
+        //nova matriz auxiliar que não possui a coluna com chave
+        double[][] aux=new double[itemVendaUnico.length][itemVendaUnico[0].length-1];
+        //remove the column key's column
+        //retira a coluna que possui chave 
+        for(int i=0; i<itemVendaUnico.length; i++){
+            int k=0;
+            for(int j=0; j<itemVendaUnico[i].length-1; k++,j++){
+                if(k==pos2){
+                    k++;
+                }
+                aux[i][j]=itemVendaUnico[i][k];
+            }
+        }
+        //new result matrix. Don't has 'null' data
+        //nova matriz resposta, porém possui campos em branco 
+        double[][] resp=new double[aux.length][aux[0].length];
+        int resultLength=0;
+        
+        for(int i=0; i<aux.length; i++){
+            int contadorPartic=0;//numero de vezes de repetições de linha
+            for(int j=i; j<aux.length; j++){
+                int contadorColunas=0;
+                    for(int k=0; k<aux[i].length; k++){
+                        if(aux[i][k]==aux[j][k]){
+                            contadorColunas++;//numero de colunas da linha que são semelhantes
+                        }
+                        if(contadorColunas==aux[0].length){//verifica se todas linhas são iguais, acresce repetições
+                            contadorPartic++;
+                        }
+                        if(contadorPartic==chaveUnica.length){
+                            resp[resultLength]=aux[i];
+                            resultLength++;
+                            j=aux.length;
+                        }
+                }
+            }
+        }
         //copy result matrix in correct lenght
-        double[][] result=new double[0][0];
+        //copia matriz resposta no tamanho correto
+        double[][] result=new double[resultLength][aux[0].length];
+        for(int i=0; i<result.length; i++) {
+            result[i]=resp[i];
+        }
         return result;
-    } 
-    
+    }
 }
